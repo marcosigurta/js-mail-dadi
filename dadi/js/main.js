962d13874a55291c52d37d5acfd81868e718a64a
marco <marcosigurta.ovb@gmail.com>
2020-06-23 15:19:34 +0200

    Added Img

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -1,3 +1,12 @@
+var diceImg = [
+    '<img src="img/1.png" alt="" />',
+    '<img src="img/2.png" alt="" />',
+    '<img src="img/3.png" alt="" />',
+    '<img src="img/4.png" alt="" />',
+    '<img src="img/5.png" alt="" />',
+    '<img src="img/6.png" alt="" />',
+    '<img src="img/7.png" alt="" />'
+]
 
 var btn = document.getElementById('btn').addEventListener("click", function () {
     var dadoUtente = Math.floor(Math.random() * 6) + 1;
@@ -11,5 +20,15 @@ var btn = document.getElementById('btn').addEventListener("click", function () {
         document.getElementById('result').innerHTML = "Ha vinto il PC!"
     } else {
         document.getElementById('result').innerHTML = "Pari!"
+    }
+
+    for (var x = 0; x <= 6; x++) {
+        if (dadoCpu == x)
+        document.getElementById('dice2Img').innerHTML = diceImg [x - 1];
+    }
+
+    for (var i = 0; i <= 6; i++) {
+        if (dadoUtente == i)
+        document.getElementById('dice1Img').innerHTML = diceImg [i - 1];
     }
 })
